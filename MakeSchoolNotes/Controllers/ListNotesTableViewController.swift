a3869c023f84c871121e5f7a75b4b29af9392d6b
ahmed3m <ahmed_3m2010@hotmail.com>
2016-06-24 15:57:27 -0400

    Add Note.swift file
    Note.swift file will allow the table view to interact with the user's
    notes instead of it being hard-coded.

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,32 +10,30 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
 
+  var notes = [Note]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
-  // 1
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return notes.count
   }
   
-  // 2
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//    // 3
-//    let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
-//    
-//    // 4
-//    cell.textLabel?.text = "Yay - it's working!"
-//    
-//    // 5
-//    return cell
+    let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath) as! ListNotesTableViewCell
     
     // 1
-    let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath) as! ListNotesTableViewCell  // downcasting
+    let row = indexPath.row
     
     // 2
-    cell.noteTitleLabel.text = "note's title"
-    cell.noteModificationTimeLabel.text = "note's modification time"
+    let note = notes[row]
+    
+    // 3
+    cell.noteTitleLabel.text = note.title
+    
+    // 4
+    cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
     
     return cell
   }
---
+++ MakeSchoolNotes/Models/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  MakeSchoolNotes
+//
+//  Created by Ahmed Abdelrahman on 6/24/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+  var title = ""
+  var content = ""
+  var modificationTime = NSDate()
+}
